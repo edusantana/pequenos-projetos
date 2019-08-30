b35de3ca6c355a62af2cec700e582a818366b8a1
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@gmail.com>
2019-08-30 11:17:16 -0300

    fogao

--- cli/click/fogao.py
+++ cli/click/fogao.py
@@ -2,7 +2,7 @@ import click
 
 @click.command()
 @click.option('--liga/--desliga', help="Liga ou desliga", default=True, show_default=True)
-@click.option('--boca', 'boca', type=int, help="Numero da boca do fogao a ser ligada. Use 0 para o forno.", required=True)
+@click.option('-b','--boca', 'boca', type=int, help="Numero da boca do fogao a ser ligada. Use 0 para o forno.", required=True)
 @click.option('-i','--intensidade',  'intensidade', help="Intensidade do fogo.", default="medio", show_default=True, type=click.Choice(['baixo', 'medio', 'alto']))
 def hello(liga, boca, intensidade):
     if liga:
