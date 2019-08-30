a4598a7cf3abb2abaf2e399e66b30892133053e8
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@gmail.com>
2019-08-30 10:45:05 -0300

    cli

--- cli/click/fogao.py
+++ cli/click/fogao.py
@@ -3,7 +3,7 @@ import click
 @click.command()
 @click.option('--liga/--desliga', help="Liga ou desliga")
 @click.option('--boca', 'boca', type=int)
-@click.option('-i','--intensidade', 'intensidade', help="Intensidade: baixo|medio|alto")
+@click.option('-i','--intensidade', default='baixo', 'intensidade', help="Intensidade: baixo|medio|alto")
 def hello(liga, boca, intensidade):
     if liga:
         click.secho('Ligando...', fg='green')
