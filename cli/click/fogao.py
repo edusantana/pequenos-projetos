780036310ec3614073763f84c80d8124e54bd8d7
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@gmail.com>
2019-08-30 10:12:55 -0300

    click

---
+++ cli/click/fogao.py
@@ -0,0 +1,17 @@
+import click
+
+@click.command()
+@click.option('--liga/--desliga', help="Liga ou desliga")
+@click.option('--boca', 'boca', type=int)
+@click.option('-i','--intensidade', 'intensidade', help="Intensidade: baixo|medio|alto")
+def hello(liga, boca, intensidade):
+    if liga:
+        click.secho('Ligando...', fg='green')
+        if boca:
+            click.echo('boca %s com intensidade %s' % (boca, intensidade))
+    else:
+        click.secho('Desligando...', fg='red')
+        click.echo('boca %s' % (boca))
+
+if __name__ == '__main__':
+    hello()
