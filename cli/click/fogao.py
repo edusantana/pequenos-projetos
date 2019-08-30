fb381f55eb1f7d9a839cb9dfb2e163c8b65264ee
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@gmail.com>
2019-08-30 11:14:09 -0300

    fogao

--- cli/click/fogao.py
+++ cli/click/fogao.py
@@ -1,16 +1,16 @@
 import click
 
 @click.command()
-@click.option('--liga/--desliga', help="Liga ou desliga")
-@click.option('--boca', 'boca', type=int,help="Numero da boca do fogao a ser ligada, ex: 3")
-@click.option('-i','--intensidade',  'intensidade', help="Intensidade: baixo|medio|alto", default="medio")
+@click.option('--liga/--desliga', help="Liga ou desliga", default=True, show_default=True)
+@click.option('--boca', 'boca', type=int, help="Numero da boca do fogao a ser ligada. Use 0 para o forno.", required=True)
+@click.option('-i','--intensidade',  'intensidade', help="Intensidade do fogo.", default="medio", show_default=True, type=click.Choice(['baixo', 'medio', 'alto']))
 def hello(liga, boca, intensidade):
     if liga:
         click.secho('Ligando...', fg='green')
-        if boca:
-            click.echo('boca %s com intensidade %s' % (boca, intensidade))
+        if boca == 0:
+            click.echo('o forno %s com intensidade %s' % (boca, intensidade))
         else:
-            click.secho('Faltou especificar a boca %s' % (boca), fg='red')
+            click.echo('boca %s com intensidade %s' % (boca, intensidade))
     else:
         click.secho('Desligando...', fg='red')
         click.echo('boca %s' % (boca))
